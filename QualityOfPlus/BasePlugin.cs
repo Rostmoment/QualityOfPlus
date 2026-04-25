0510ae55e39c25368ddd3ee29681a00358b619c0
Rost Moment <110627353+Rostmoment@users.noreply.github.com>
2026-04-25 12:00:20 +0300

    Changed version

--- QualityOfPlus/BasePlugin.cs
+++ QualityOfPlus/BasePlugin.cs
@@ -30,7 +30,7 @@ namespace QualityOfPlus
     {
         public const string NAME = "Quality Of Plus";
         public const string GUID = "rost.moment.baldiplus.qop";
-        public const string VERSION = "1.9.1";
+        public const string VERSION = "1.9.2";
     }
 
     [BepInDependency(MTM101BaldiDevAPI.ModGUID, BepInDependency.DependencyFlags.HardDependency)]
